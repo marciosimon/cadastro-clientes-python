b38a00edfd174597268ea8fe915936a181064a65
MARCIO SIMON DE FREITAS <marcio.freitas-ext@viavarejo.com.br>
2020-12-03 20:29:17 -0300

    Exemplo de como consumir api

--- clientes/consume_api_product.py
+++ clientes/consume_api_product.py
@@ -1,11 +1,11 @@
 import requests
+#from clientes.models import Cliente,Produto
 
-url = "http://challenge-api.luizalabs.com/api/product/?page=1"
-response = requests.get(url)
+response = requests.get("http://challenge-api.luizalabs.com/api/product/?page=1")
+products = response.json()
 
-if (response.status_code == 200):
-    print('Json', response.json())
-else:
-    print('Status Code', response.status_code)
-    print('Reason', response.reason)
-    print('Text', response.text)
+print(products)
+
+for product in products['products']:
+    #Produto.objects.create(title=product['title'])
+    print(product['title'])
